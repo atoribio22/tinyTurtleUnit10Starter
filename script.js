d68ba9b53dbb5a62c145a7d795fe323192ac728a
Ashley Toribio <atoribio@aclanyc.com>
2017-03-24 12:22:42 +0000

    Tiny Turtle 2

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
+/*global TinyTurtle*/
+/*global forward*/
+/*global right*/
+/*global stamp*/
+/*global left*/
+
+
+var len = 50;
 
 function square(){
-forward(50);
+right(30);
+forward(len);
 right(90);
-forward(50);
+forward(len);
 right(90);
-forward(50);
+forward(len);
 right(90);
-forward(50);
+forward(len);
 right(90);
-forward(50);
+forward(len);
 }
 
-// Type your function call below
-
+// Triangle function
+function triangle(){
+right(270);
+forward(len);
+right(120);
+forward(len);
+right(120);
+forward(len);
+}
 
 stamp();
+triangle(len);
+square(len);
+
+
+// Hexagon function
+var size =50 
+function hexagon(){
+    right(60);
+    forward(size);
+    left(60);
+    forward(size);
+    left(60);
+    forward(size);
+    left(60);
+    forward(size);
+    left(60);
+    forward(size);
+    left(60);
+    forward(size);
+}
+
+
+hexagon(size);
+
+// function house(){
+//     triangle(size);
+//     square(size);
+// }
+
+// stamp();
+// house(size);
+
+
+
+
+
